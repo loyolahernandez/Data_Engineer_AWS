efb573a01bcc587f0615068af4064eae37f0a225
Ignacio Loyola <ignacio.loyola@biomedica.udec.cl>
2024-08-19 12:02:56 -0400

    utils.py SurfWatch

--- Proyecto1/SurfWatch_Scripts/utils.py
+++ Proyecto1/SurfWatch_Scripts/utils.py
@@ -8,61 +8,38 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
+def compare_conditions(df, spotId, wind, max_height, min_height, sunrise, sunset):
+    df_sesh = df[
+        (df['surf_max'] <= max_height) & (df['surf_max'] >= min_height) &
+        (df['timestamp_dt'].dt.hour <= sunset) & (df['timestamp_dt'].dt.hour >= sunrise) &
+        (df['directionType'] == wind)
+    ].copy()
 
+    return df_sesh
 
-def get_date():
+def create_sms(df_sesh):
+    df_sesh['date'] = df_sesh['timestamp_dt'].dt.date
+    df_sesh['hour'] = df_sesh['timestamp_dt'].dt.hour
 
-    input_date = datetime.now()
-    input_date = input_date.strftime("%Y-%m-%d")
+    # Data para el sms
+    df_sms = df_sesh[['hour', 'surf_max', 'directionType']]
 
-    return input_date
+    # SMS a string
+    sms = df_sms.to_string(index=False)
 
-def request_wapi(api_key,query):
+    return sms
 
-    url_clima = 'http://api.weatherapi.com/v1/forecast.json?key='+api_key+'&q='+query+'&days=1&aqi=no&alerts=no'
-
-    try :
-        response = requests.get(url_clima).json()
-    except Exception as e:
-        print(e)
-
-    return response
-
-def get_forecast(response,i):
-
-    fecha = response['forecast']['forecastday'][0]['hour'][i]['time'].split()[0]
-    hora = int(response['forecast']['forecastday'][0]['hour'][i]['time'].split()[1].split(':')[0])
-    condicion = response['forecast']['forecastday'][0]['hour'][i]['condition']['text']
-    tempe = response['forecast']['forecastday'][0]['hour'][i]['temp_c']
-    rain = response['forecast']['forecastday'][0]['hour'][i]['will_it_rain']
-    prob_rain = response['forecast']['forecastday'][0]['hour'][i]['chance_of_rain']
-
-    return fecha,hora,condicion,tempe,rain,prob_rain
-
-def create_df(data):
-
-    col = ['Fecha','Hora','Condicion','Temperatura','Lluvia','prob_lluvia']
-    df = pd.DataFrame(data,columns=col)
-    df = df.sort_values(by = 'Hora',ascending = True)
-
-    df_rain = df[(df['Lluvia']==1) & (df['Hora']>6) & (df['Hora']< 22)]
-    df_rain = df_rain[['Hora','Condicion']]
-    df_rain.set_index('Hora', inplace = True)
-
-    return df_rain
-
-def send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,df,query):
-
-    account_sid = TWILIO_ACCOUNT_SID
+def send_sms():
+    account_sid = TWILIO_ACCOUNT_SID 
     auth_token = TWILIO_AUTH_TOKEN
 
     client = Client(account_sid, auth_token)
 
     message = client.messages \
                     .create(
-                        body='\nHola! \n\n\n El pronostico de lluvia hoy '+ input_date +' en ' + query +' es : \n\n\n ' + str(df),
+                        body='\nHola! \n\n\n Hoy puede salir ola en Buchu. \n\n\nCondiciones esperadas: ' + wave_sms(spotId, wind, max_height, min_height, sunrise, sunset),
                         from_=PHONE_NUMBER,
                         to='+56982091549'
                     )
-
+    
     return message.sid
